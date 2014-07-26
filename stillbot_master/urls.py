b46c3b8c8f0e43b5baf4d6be096ad4db8916faf8
paul m <paul.mackin@gmail.com>
2014-07-27 05:01:27 +1000

    added D3 graph proof-of-concept as part of ongoing (read: drawn out) learning experience of drawing real-time graphs; added grappelli

--- stillbot_master/urls.py
+++ stillbot_master/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.models import User, Group
+from webapp.models import Thermistor, Temperature
+from django.views.generic import TemplateView
 from rest_framework import viewsets, routers
 from django.contrib import admin
 admin.autodiscover()
@@ -11,6 +13,12 @@ class UserViewSet(viewsets.ModelViewSet):
 class GroupViewSet(viewsets.ModelViewSet):
     model = Group
 
+class TemperatureViewSet(viewsets.ModelViewSet):
+    model = Temperature
+
+class ThermistorViewSet(viewsets.ModelViewSet):
+    model = Thermistor
+
 # Routers provide an easy way of automatically determing the URL conf.
 #
 # Issue: Could not resolve URL for hyperlinked relationship using view name "permission-detail"
@@ -19,6 +27,8 @@ class GroupViewSet(viewsets.ModelViewSet):
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'gropus', GroupViewSet)
+router.register(r'temperatures', TemperatureViewSet)
+router.register(r'thermistors', ThermistorViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browseable API.
@@ -26,7 +36,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'stillbot_master.views.home', name='home'),
 
-    url(r'^', include(router.urls)),
+    url(r'^api/v1/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^dashboard', TemplateView.as_view(template_name='dashboard.html')),
+    #url(r'^grappelli', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
